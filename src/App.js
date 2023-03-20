81660bc49c6c031a79db9ed2ff78a1e82029ff57
polketchaba <polketch97@gmail.com>
2023-03-20 17:38:53 +0100

    c5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-//import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-//import Projects from "./pages/Projects";
-//import Experience from "./pages/Experience";
-//import Bildung from "./pages/Bildung";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Bildung from "./pages/Bildung";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-//import Contact from "./pages/Contact";
-//import ProjectDisplay from "./pages/ProjectDisplay";
+import Contact from "./pages/Contact";
+import ProjectDisplay from "./pages/ProjectDisplay";
 
 
 function App() {
@@ -18,6 +17,11 @@ function App() {
       <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/bildung" element={<Bildung />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       <Footer />
       </Router>
