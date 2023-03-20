ceab4d35239e29035dbeaf94b62f1105b0099036
polketchaba <polketch97@gmail.com>
2023-03-20 17:25:13 +0100

    c4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 //import Projects from "./pages/Projects";
 //import Experience from "./pages/Experience";
 //import Bildung from "./pages/Bildung";
-//import Navbar from "./components/Navbar";
-//import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 //import Contact from "./pages/Contact";
 //import ProjectDisplay from "./pages/ProjectDisplay";
 
@@ -14,7 +14,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hi
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      <Footer />
+      </Router>
     </div>
   );
 }
